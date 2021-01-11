e50f131e7da16ed2e13971a023179283a5b3c5e7
Lindsay Morris <lindsaymorris813@gmail.com>
2021-01-10 19:09:02 -0500

    initial commit

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,10 @@
+function Jumbotron() {
+    return (
+        <div className="jumbotron">
+        <h1>Employee Directory</h1>
+        <p>Search for Colleagues</p>
+    </div>
+    );
+}
+
+export default Jumbotron;
